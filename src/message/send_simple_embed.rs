5bc8c0e10df1c315978d40ce7c52b073f55c83aa
keiveulbugs <114590495+keiveulbugs@users.noreply.github.com>
2024-07-19 23:44:18 +0200

    send_simple_embed now with cachehttp / serenity context instead of poise

--- src/message/send_simple_embed.rs
+++ src/message/send_simple_embed.rs
@@ -1,13 +1,13 @@
 /// Send a simple embed.
 pub async fn reply_simple_embed<U, E>(
-    ctx: poise::Context<'_, U, E>,
+    cache_http: impl serenity::all::CacheHttp,
     channelid: serenity::all::ChannelId,
     title: impl Into<String>,
     description: impl Into<String>,
 ) -> Result<serenity::all::Message, serenity::Error> {
     let message = channelid
         .send_message(
-            ctx.serenity_context(),
+            cache_http,
             serenity::all::CreateMessage::default().embed(
                 serenity::all::CreateEmbed::default()
                     .title(title)
