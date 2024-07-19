13b83dca5eea29be43a4b6a1c373c5d48514c806
keiveulbugs <114590495+keiveulbugs@users.noreply.github.com>
2024-07-19 23:26:38 +0200

    admincheck

---
+++ src/permission/admincheck.rs
@@ -0,0 +1,7 @@
+/// Check if the invoker of the context is an admin of that guild.
+pub async fn admincheck<U, E>(ctx: poise::Context<'_, U, E>) -> bool {
+    ctx.author_member()
+        .await
+        .and_then(|m| m.permissions)
+        .is_some_and(poise::serenity_prelude::Permissions::administrator)
+}
